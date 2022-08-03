72f56df148c2be00db10d1977d9365ada25521e8
bagus25dzikri06 <bagusdzikri@gmail.com>
2022-08-03 15:08:20 +0700

    Update for Task 2 in function without putting the name array

--- task2.js
+++ task2.js
@@ -1,17 +1,21 @@
 const isString = (currentValue) => typeof currentValue === 'string' === true
+const name = [
+  'Abigail', 'Alexandra', 'Alison', 'Amanda', 
+  'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn',
+  'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 
+  'Olivia', 'Penelope'
+]
 
-const searchName = (nameSearchQuery, nameTotal, nameData, sliceArr) => {
+const searchName = (nameSearchQuery, nameTotal, sliceArr) => {
   try {
     if (typeof nameSearchQuery === 'string' === false) throw 'Input query cari nama harus berupa string'
     if (typeof nameTotal === 'number' === false) throw 'Input total nama yang dicari harus berupa angka'
-    if (typeof nameData === 'object' === false) throw 'Input data nama harus berupa array'
-    if (nameTotal > nameData.length) throw 'Maaf, total nama yang dicari melebihi jumlah data nama'
-    if (nameData.every(isString) === false) throw 'Input setiap data nama harus berupa string'
+    if (nameTotal > name.length) throw 'Maaf, total nama yang dicari melebihi jumlah data nama'
 
     let nameSearchResult = []
-    for (let i = 0; i < nameData.length; i++) {
-      if (nameData[i].toLowerCase().includes(nameSearchQuery) === true) {
-        nameSearchResult.push(nameData[i])
+    for (let i = 0; i < name.length; i++) {
+      if (name[i].toLowerCase().includes(nameSearchQuery) === true) {
+        nameSearchResult.push(name[i])
       }
     }
 
@@ -26,23 +30,4 @@ const sliceArr = (arr, num) => {
   return arr.slice(0, num)
 }
 
-console.log(searchName('an', 3, [
-  'Abigail', 'Alexandra', 'Alison', 'Amanda', 
-  'Angela', 'Bella', 'Carol', 'Caroline', 'Carolyn',
-  'Deirdre', 'Diana', 'Elizabeth', 'Ella', 'Faith', 
-  'Olivia', 'Penelope'
-], sliceArr))
-console.log(searchName('wah', 3, [
-  'Christian', 'Budi', 'Iman', 'Danang', 'Bima'
-], sliceArr))
-console.log(searchName('ji', 8, [
-  'Deni', 'Aji', 'Hanafi', 'Sundari', 'Priyo'
-], sliceArr))
-console.log(searchName('sa', 10, 'Lukman, Ali, Dadang, Sarah, Sahat, Binsar', sliceArr))
-console.log(searchName('ji', '9', [
-  'Neni', 'Wawan', 'Henry', 'Danar', 'Kustian', 'Wahyu'
-], sliceArr))
-console.log(searchName(2, 2, [
-  'Herry', 'Melly', 'William', 'Fauzi', 
-  'Damar', 'Ikhsan', 'Ridwan'
-], sliceArr))
+console.log(searchName('an', 3, sliceArr))
